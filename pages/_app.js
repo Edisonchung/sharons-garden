29eb05040202f5380c1dfbe8c5c7c272a8a638b5
Edisonchung <Edisonchung@users.noreply.github.com>
2025-05-24 04:07:59 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@
 import Navbar from '../components/ui/Navbar';
 import '../styles/globals.css';
 import '../styles/canvas.css'; // ✅ Import canvas styles globally
+import '../styles/FlowerCanvas.css';
+
 
 export default function MyApp({ Component, pageProps }) {
   return (
