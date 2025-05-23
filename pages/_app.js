ff08aa6c760823a3fa4ba304971138a83eeb3430
Edisonchung <Edisonchung@users.noreply.github.com>
2025-05-24 03:48:05 +0800

    Update _app.js
    add canvas.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
+// pages/_app.js
 import Navbar from '../components/ui/Navbar';
 import '../styles/globals.css';
+import '../styles/canvas.css'; // ✅ Import canvas styles globally
+
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
