d84431860177d5ee91e5aede02cd95339a21df33
Edisonchung <Edisonchung@users.noreply.github.com>
2025-05-24 18:34:33 +0800

    Update input.js

--- components/ui/input.js
+++ components/ui/input.js
@@ -1,4 +1,11 @@
 
-export function Input({ className = '', ...props }) {
-  return <input className={`border p-2 rounded w-full ${className}`} {...props} />;
+// components/ui/input.js
+export function Input({ type = 'text', ...props }) {
+  return (
+    <input
+      type={type}
+      className="p-2 border border-gray-300 rounded dark:bg-gray-700 dark:border-gray-600 w-full"
+      {...props}
+    />
+  );
 }
