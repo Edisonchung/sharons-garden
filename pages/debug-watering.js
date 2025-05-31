c5b97a53d317b9c11c9cf6c20dfcf3bb6c5702e8
Edisonchung <Edisonchung@users.noreply.github.com>
2025-06-01 00:22:34 +0800

    Create debug-watering.js

---
+++ pages/debug-watering.js
@@ -0,0 +1,6 @@
+// In a test page like pages/debug-watering.js
+import WateringDebugTest from '../components/WateringDebugTest';
+
+export default function DebugWateringPage() {
+  return <WateringDebugTest />;
+}
